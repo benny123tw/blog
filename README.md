f8a05beebb5e7e1d028df7b05ac74781afdc0b94
Benny Yen <benny123tw@gmail.com>
2022-08-10 22:58:47 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Benny's Blog
+
+blog building with [deno_blog](https://github.com/denoland/deno_blog).
+
+## Running
+
+```sh
+deno task dev
+```
---
+++ extension/prismjs-extensions.ts
@@ -0,0 +1,4 @@
+import "https://esm.sh/prismjs@1.28.0/components/prism-typescript";
+import "https://esm.sh/prismjs@1.28.0/components/prism-ruby";
+import "https://esm.sh/prismjs@1.28.0/components/prism-python";
+import "https://esm.sh/prismjs@1.28.0/components/prism-go";
